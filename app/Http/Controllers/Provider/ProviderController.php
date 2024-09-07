9a409f2e627c75e9a1dd6d0714a77055d78f0202
VenzTheGreatHD <venzolarte@gmail.com>
2024-09-08 01:12:20 +0800

    [update] added supabase as DB

--- app/Http/Controllers/Provider/ProviderController.php
+++ app/Http/Controllers/Provider/ProviderController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers\Provider;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class ProviderController extends Controller
 {
@@ -15,23 +13,24 @@ class ProviderController extends Controller
         return Socialite::driver($provider)->redirect();
     }
 
-    public function callback ($provider)
+    public function callback($provider)
     {
         $SocialUser = Socialite::driver($provider)->user();
 
         //dd($SocialUser);
 
-        $user = User::updateOrCreate([
-            'provider_id' => $SocialUser->id,
-            'provider' => $provider,
-        ],
-         [
-            'name' => $SocialUser->name,
-            'email' => $SocialUser->email,
-             'username' => User::generateUsername($SocialUser->nickname),
-            'provider_token' => $SocialUser->token,
-            'provider_refresh_token' => $SocialUser->refreshToken,
-        ]);
+        # TODO: PUT THIS ON THE ACCOUNT CREATION, NOT LOGIN
+//        $user = User::updateOrCreate([
+//            'provider_id' => $SocialUser->id,
+//            'provider' => $provider,
+//        ],
+//         [
+//            'name' => $SocialUser->name,
+//            'email' => $SocialUser->email,
+//             'username' => User::generateUsername($SocialUser->nickname),
+//            'provider_token' => $SocialUser->token,
+//            'provider_refresh_token' => $SocialUser->refreshToken,
+//        ]);
 
         Auth::login($user);
 
